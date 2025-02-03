332214a534caa153a458350cc7c1c551e9acee5c
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-03 15:37:04 -0300

    Update horasextras.test.js

--- horasextras.test.js
+++ horasextras.test.js
@@ -3,3 +3,7 @@ const fnHorasExtras = require('./horasextras');
 test ('salario por hora 10, horas trabalhadas 50, carga horario 40, resultado experado 100', () => { 
     expect(fnHorasExtras(10, 50, 40)).toBe(100);
 })
+
+test ('salario por hora 10, horas trabalhadas 30, carga horario 36, resultado experado 0', () => { 
+    expect(fnHorasExtras(10, 30, 36)).toBe(0);
+})
