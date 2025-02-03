89db7c66a7ead42459759b2419daad978695a69d
guilherme-costa14 <143550872+guilherme-costa14@users.noreply.github.com>
2025-02-03 17:08:45 -0300

    desenvolvimento

--- horasextras.js
+++ horasextras.js
@@ -1,9 +1,16 @@
 function fnHorasExtras(salario, horastrabalhadas, cargahoraria) {
     let horasextras = 0
-    if(horastrabalhadas > cargahoraria){
-        horasextras = (horastrabalhadas - cargahoraria) * salario
+    if(isNaN(salario) || isNaN(horastrabalhadas) || isNaN(cargahoraria)){
+        return "todos os valores devem ser números"
     }
 
+    if (salario < 0 || horastrabalhadas < 0 || cargahoraria < 0) {
+        return "todos os valores devem ser positivos"
+    } 
+
+    if (horastrabalhadas > cargahoraria) {
+        horasextras = (horastrabalhadas - cargahoraria) * salario
+    }
     return horasextras
 }
 
--- horasextras.test.js
+++ horasextras.test.js
@@ -7,3 +7,27 @@ test ('salario por hora 10, horas trabalhadas 50, carga horario 40, resultado ex
 test ('salario por hora 10, horas trabalhadas 30, carga horario 36, resultado experado 0', () => { 
     expect(fnHorasExtras(10, 30, 36)).toBe(0);
 })
+
+test ('salario por hora -10, Horas trabalhadas 50, carga horaria 40, resultado esperado todos os valores devem ser positivos', () => { 
+    expect(fnHorasExtras(-10, 50, 40)).toBe("todos os valores devem ser positivos");
+})
+
+test ('salario por hora 10, Horas trabalhadas -50, carga horaria 40, resultado esperado todos os valores devem ser positivos', () => { 
+    expect(fnHorasExtras(10, -50, 40)).toBe("todos os valores devem ser positivos");
+})
+
+test ('salario por hora 10, Horas trabalhadas 50, carga horaria -40, resultado esperado todos os valores devem ser positivos', () => { 
+    expect(fnHorasExtras(10, 50, -40)).toBe("todos os valores devem ser positivos");
+})
+
+test ('salario por hora "A", Horas trabalhadas 50, carga horaria -40, resultado esperado todos os valores devem números', () => { 
+    expect(fnHorasExtras("A", 50, -40)).toBe("todos os valores devem ser números");
+})
+
+test ('salario por hora 10, Horas trabalhadas "A", carga horaria -40, resultado esperado todos os valores devem números', () => { 
+    expect(fnHorasExtras(10, "A", -40)).toBe("todos os valores devem ser números");
+})
+
+test ('salario por hora 10, Horas trabalhadas 50, carga horaria "A", resultado esperado "todos os valores devem números', () => { 
+    expect(fnHorasExtras(10, 50, "A")).toBe("todos os valores devem ser números");
+})
